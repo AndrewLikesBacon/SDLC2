77514914b5d8f15f2a8fbfc1e44e457e60c522a2
AndrewLikesBacon <105609773+AndrewLikesBacon@users.noreply.github.com>
2022-07-10 17:38:58 -0400

    Add files via upload

---
+++ module-info.java
@@ -0,0 +1,8 @@
+module MyJavaFx {
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires org.junit.jupiter.api;
+	requires junit;
+	
+	opens application to javafx.graphics, javafx.fxml;
+}
